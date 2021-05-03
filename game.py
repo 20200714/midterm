c7e8669f54ac33653edff0734e8bdceef5cd6475
bsh <20200714@g.kit.ac.kr>
2021-05-03 16:15:45 +0900

    시험제출

---
+++ game.py
@@ -0,0 +1,13 @@
+print("테트리스 시작")
+list_a = ("1. 블록 바꾸기", "2. 우측 이동", "3. 좌측 이동")
+print(list_a)
+dict_a = {1 : list_a[0],2 : list_a[1],3 : list_a[2]}
+num = int(input("선택 : "))
+if num ==1:
+    print(dict_a[1])
+elif num == 2:
+    print(dict_a[2])
+elif num == 3:
+    print(dict_a[3])
+else :
+    print("숫자를 입력하세요.")
